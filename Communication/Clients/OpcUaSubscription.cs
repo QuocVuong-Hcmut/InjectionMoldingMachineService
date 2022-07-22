1a01b4d12e8f7fdb0593efc028a3a856abb614b6
Thuyen Le <thuyen.le1999@gmail.com>
2022-07-22 10:50:53 +0700

    Initialize repository

---
+++ Communication/Clients/OpcUaSubscription.cs
@@ -0,0 +1,55 @@
+﻿using Opc.Ua;
+using Opc.Ua.Client;
+
+namespace InjectionMoldingMachineDataAcquisitionService.Communication.Clients;
+public class OpcUaSubscription
+{
+    private readonly Subscription _subscription;
+    private readonly List<OpcUaNotificationHandler> notificationHandlers = new();
+
+    public OpcUaSubscription(Subscription subscription)
+    {
+        _subscription = subscription;
+    }
+
+    public void AddMonitorItem(string nodeId, string itemName, int samplingInterval, List<Action<MetricMessage>> handler)
+    {
+        MonitoredItem item = new(_subscription.DefaultItem)
+        {
+            DisplayName = itemName,
+            StartNodeId = new NodeId(nodeId),
+            AttributeId = Attributes.Value,
+            SamplingInterval = samplingInterval
+        };
+
+        var notificationHandler = new OpcUaNotificationHandler(handler, item);
+
+        _subscription.AddItem(item);
+        _subscription.ApplyChanges();
+
+        notificationHandlers.Add(notificationHandler);
+    }
+
+
+    private class OpcUaNotificationHandler
+    {
+        private event Action<MetricMessage>? _messageHandlers;
+
+        public OpcUaNotificationHandler(List<Action<MetricMessage>> messageHandler, MonitoredItem monitoredItem)
+        {
+            messageHandler.ForEach(h => _messageHandlers += h);
+
+            monitoredItem.Notification += HandleNotification;
+        }
+
+        private void HandleNotification(MonitoredItem monitoredItem, MonitoredItemNotificationEventArgs e)
+        {
+            MonitoredItemNotification? notification = e.NotificationValue as MonitoredItemNotification;
+
+            if (notification is not null)
+            {
+                _messageHandlers?.Invoke(new MetricMessage(monitoredItem.DisplayName, notification.Value.Value, notification.Value.SourceTimestamp));
+            }
+        }
+    }
+}
---
+++ Domain/InjectionMoldingMachines/KebaInjectionMoldingMachine.cs
@@ -0,0 +1,107 @@
+﻿using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace InjectionMoldingMachineDataAcquisitionService.Domain.InjectionMoldingMachines;
+public class KebaInjectionMoldingMachine
+{
+    public bool IsConnected => _opcUaClient.IsConnected;
+    public string MachineId { get; set; }
+
+    private readonly OpcUaClient _opcUaClient;
+    private readonly CycleDataToCsvAppender _cycleDataToCsvAppender;
+    private readonly Timer _reconnectTimer;
+    private readonly IBusControl _busControl;
+
+    private string? moldId;
+    private double? configuredCycle;
+    private DateTime currentDoorOpenTime;
+    private DateTime currentDoorCloseTime;
+
+    public KebaInjectionMoldingMachine(OpcUaClient opcUaClient, CycleDataToCsvAppender cycleDataToCsvAppender, string machineId, IBusControl busControl)
+    {
+        _opcUaClient = opcUaClient;
+        _cycleDataToCsvAppender = cycleDataToCsvAppender;
+        this.MachineId = machineId;
+
+        _reconnectTimer = new Timer(10000);
+        _reconnectTimer.Elapsed += ReconnectTimerElapsed;
+        _busControl = busControl;
+    }
+
+    public async Task Connect()
+    {
+        _reconnectTimer.Enabled = false;
+        try
+        {
+            await _opcUaClient.ConnectAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{MachineId}: Connection failed. {ex.Message}");
+            _reconnectTimer.Enabled = true;
+            return;
+        }
+
+        var subscription = _opcUaClient.Subscribe(1000);
+
+        subscription.AddMonitorItem("ns=4;s=APPL.system.sv_CycleTime_KVB", "CycleTime", 1000, new List<Action<MetricMessage>>() {
+            PublishMetricMessage,
+            HandleInjectionCycle });
+        subscription.AddMonitorItem("ns=4;s=SYS.IO.ONBOARD.DI:40.value", "DoorOpenned", 1000, new List<Action<MetricMessage>>() {
+            PublishMetricMessage,
+            HandleDoorOpened });
+    }
+
+    public void SetMoldId(string moldId)
+    {
+        this.moldId = moldId;
+    }
+
+    public void SetCycle(double configuredCycle)
+    {
+        this.configuredCycle = configuredCycle;
+    }
+
+    private void PublishMetricMessage(MetricMessage metricMessage)
+    {
+        _busControl.Publish<UaMessage>(new UaMessage(metricMessage.Name, metricMessage.Value));
+    }
+
+    private void HandleDoorOpened(MetricMessage metricMessage)
+    {
+        var timestamp = metricMessage.Timestamp;
+        bool doorOpened = !((byte)metricMessage.Value == 0);
+        if (doorOpened)
+        {
+            currentDoorOpenTime = timestamp;
+        }
+        else
+        {
+            currentDoorCloseTime = timestamp;
+        }
+
+    }
+
+    public void HandleInjectionCycle(MetricMessage metricMessage)
+    {
+        var timestamp = metricMessage.Timestamp;
+        var cycle = (long)metricMessage.Value;
+
+        var openTime = currentDoorCloseTime.Subtract(currentDoorOpenTime);
+        var cycleTime = TimeSpan.FromTicks(cycle * 10);
+
+        _cycleDataToCsvAppender.AppendData(timestamp, cycleTime, openTime, moldId, configuredCycle);
+    }
+
+    private async void ReconnectTimerElapsed(object? sender, ElapsedEventArgs args)
+    {
+        if (!IsConnected)
+        {
+            await Connect();
+        }
+        else
+        {
+            _reconnectTimer.Enabled = false;
+        }
+    }
+}
